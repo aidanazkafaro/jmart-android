59d22cd6ac891f531d14f64169b27b36e7b40df0
aidanazkafaro <aidan.azkafaro@ui.ac.id>
2021-11-27 11:36:08 +0700

    Modul 9 - Aidan - CS jmartandroid

--- AndroidStudioProjects/jmartandroid/app/src/main/java/AidanAzkafaroDesonJmartFH/jmart_android/MainActivity.java
+++ AndroidStudioProjects/jmartandroid/app/src/main/java/AidanAzkafaroDesonJmartFH/jmart_android/MainActivity.java
@@ -3,6 +3,8 @@ package AidanAzkafaroDesonJmartFH.jmart_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView userMain = (TextView) findViewById(R.id.greetingText);
+        userMain.setText("Hello" + LoginActivity.getLoggedAccount().name + "!");
     }
 }
---
+++ AndroidStudioProjects/jmartandroid/app/src/main/java/AidanAzkafaroDesonJmartFH/jmart_android/model/Account.java
@@ -0,0 +1,10 @@
+package AidanAzkafaroDesonJmartFH.jmart_android.model;
+
+public class Account extends Serializable{
+
+    public double balance;
+    public String email;
+    public String name;
+    public String password;
+    public Store store;
+}
